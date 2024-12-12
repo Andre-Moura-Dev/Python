ccd09a2ae25dcd580e37407cf020e2bbe783ca42
Andre-Moura-Dev <andreoliveiramoura06@gmail.com>
2024-12-12 19:30:56 -0300

    Learning Python

---
+++ learningPythonDay1.py
@@ -0,0 +1,40 @@
+# Hello World!
+# print("Hello World!")
+
+# Variáveis em python
+# nome = 'Olá Mundo!'
+# nome1 = 'Meu nome é André!'
+
+# print(nome)
+# print(nome1)
+
+# Muitos valores para múltiplas variáveis
+# x, y, z = "Orange", "Banana", "Aplle"
+# print(x)
+# print(y)
+# print(z)
+
+# Um valor para múltiplas variáveis
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# Descompacta
+# fruits = ["apple", "banana", "orange"]
+# x, y, z = fruits
+
+# print(x)
+# print(y)
+# print(z)
+
+#
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+
+# x = "Python "
+# y = "is "
+# z = "awesome"
+# print(x + y + z)
---
+++ usuario.py
@@ -0,0 +1,12 @@
+nome = 'André'
+idade = 18
+
+nome = input("Informe o seu nome: ")
+idade = input("Informe a sua idade: ")
+
+if idade == 18:
+    print("Igual")
+else: idade >= 18
+print("Maior")
+
+print("Seu nome é: " + nome + "Sua idade é: "+ idade)
