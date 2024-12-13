50b4cc3802a593a124a4815e3d0c39becd0a6bf6
Andre-Moura-Dev <andreoliveiramoura06@gmail.com>
2024-12-13 16:33:43 -0300

    Learning Python

---
+++ learningPythonDay2.py
@@ -0,0 +1,46 @@
+# Data types in python
+
+# Text
+hello = "Hello World!"
+
+# Int
+x = 20
+
+# Float
+x = 20.5
+
+#Complex
+x = 1j
+
+# List
+x = ["apple", "banana", "cherry"]
+
+# Tuple
+x = ("apple", "banana", "cherry")
+
+# Range
+x = range(6)
+
+# Dict
+x = {"name" : "Jhon", "age" : 35}
+
+# Set
+x = {"apple", "banana", "cherry"}
+
+# FrozenSet
+x = frozenset({"apple", "banana", "cherry"})
+
+# Bool
+x = True
+
+# Bytes
+x = b"Hello"
+
+# ByteArray
+x = bytearray(5)
+
+# MemoryView
+x = memoryview(bytes(5))
+
+# NoneType
+x = None
