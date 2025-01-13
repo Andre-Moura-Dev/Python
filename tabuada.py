87dd1aa696416b8fc7fc2aee73ad98ea2de8015f
Andre-Moura-Dev <andreoliveiramoura06@gmail.com>
2025-01-13 15:07:19 -0300

    Tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,7 +1,9 @@
+# Função tabuada com o loop for
 def tabuada(numero):
     print(f"Tabuada do {numero}: ")
     for i in range(1,11):
         print(f"{numero} x {i} = {numero * i}")
-        
+
+# Receber o número digitado pelo usuário        
 numero_usuario = int(input("Informe um número para ver a tabuada: "))
 tabuada(numero_usuario)
