9fe7932029769760a15e8e1a161c08599c96055c
Andre-Moura-Dev <andreoliveiramoura06@gmail.com>
2025-03-11 10:01:57 -0300

    Python

---
+++ entrada6.py
@@ -0,0 +1,32 @@
+import random
+
+def jogo_pedra_papel_tesoura():
+    opcoes = ["pedra", "papel", "tesoura"]
+    
+    print("Bem-vindo ao Pedra, papel e tesoura!")
+    
+    while True:
+        jogador = input("Escolha pedra, papel ou tesoura para jogar (ou 'sair' para encerrar): ").strip().lower()
+        
+        if jogador == "sair":
+            print("Obrigado por jogar!")
+            break
+            
+        if jogador not in opcoes:
+            print("Escolha inválida! Tente novamente.")
+            continue
+        
+        computador = random.choice(opcoes)
+        print(f"O computador escolheu: {computador}")
+        
+        if jogador == computador:
+            print("Empate!")
+        elif (jogador == "pedra" and computador == "tesoura") or \
+            (jogador == "papel" and computador == "pedra") or \
+            (jogador == "tesoura" and computador == "papel"):
+                print("Voçê venceu")
+        else:
+            print("O computador venceu!")
+
+if __name__ == "main":
+    jogo_pedra_papel_tesoura()
