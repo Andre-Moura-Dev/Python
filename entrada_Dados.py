f67213d7a776dc25267a83f6b184a26e5f02fa54
Andre-Moura-Dev <andreoliveiramoura06@gmail.com>
2025-03-18 10:38:08 -0300

    Python

--- entrada_Dados.py
+++ entrada_Dados.py
@@ -1,18 +1,22 @@
+# função Entrada de dados
 def entrada():
     nome = input("Digite seu nome: ")
     idade = int(input("Digite sua idade: "))
     curso = input("Digite seu curso: ")
     return nome, idade, curso
 
+# calcular média
 def calcular_media(notas):
     if len(notas) == 0:
         return 0
     return sum(notas) / len(notas)
 
+# Sair
 def sair():
     print("Saindo do programa...")
     exit()
 
+# função principal
 def main():
     nome, idade, curso = entrada()
     notas = []
@@ -26,15 +30,23 @@ def main():
         opcao = input("Opção: ")
         
         if opcao == '1':
-            nota = float(input("Digite a nota: "))
-            notas.append(nota)
-            print(f"Nota {nota} adicionada com sucesso!")
+            quantidade = int(input("Quantas notas deseja adicionar? "))
+            for i in range(quantidade):
+                nota = float(input(f"Informe a nota {i + 1}: "))
+                notas.append(nota)
+            print(f"{quantidade} notas adicionadas com sucesso!!!")
         elif opcao == '2':
             media = calcular_media(notas)
             print(f"\nNome: {nome}")
             print(f"Idade: {idade}")
             print(f"Curso: {curso}")
             print(f"Média: {media:.2f}")
+            
+            if media >= 7:
+                print("Aprovado!")
+            else:
+                print("Reprovado!")
+            
         elif opcao == '3':
             sair()
         else:
